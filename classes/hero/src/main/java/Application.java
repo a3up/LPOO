497a0f301695a306d9231e4fec729608e8c46ff5
a3brx <a3brx@sapo.pt>
2020-02-27 18:19:20 +0000

    Using lanterna

--- classes/hero/src/main/java/Application.java
+++ classes/hero/src/main/java/Application.java
@@ -1,5 +1,24 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.*;
+import com.googlecode.lanterna.terminal.*;
+
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+
+            screen.clear();
+            screen.setCharacter(10, 10, new TextCharacter('X'));
+            screen.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
