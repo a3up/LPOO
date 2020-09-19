3a65cae02bb1b7b5973982947bff2f9bd7371a9a
a3brx <a3brx@sapo.pt>
2020-09-19 15:19:44 +0100

    last

--- labs/refactoring/src/main/java/com/aor/refactoring/example5/Turtle.java
+++ labs/refactoring/src/main/java/com/aor/refactoring/example5/Turtle.java
@@ -24,21 +24,34 @@ public class Turtle {
     }
 
     public void execute(char command) {
-        if (command == 'L') { // ROTATE LEFT
-            if (direction == 'N') direction = 'W';
-            else if (direction == 'W') direction = 'S';
-            else if (direction == 'S') direction = 'E';
-            else if (direction == 'E') direction = 'N';
-        } else if (command == 'R') { // ROTATE RIGHT
-            if (direction == 'N') direction = 'E';
-            else if (direction == 'E') direction = 'S';
-            else if (direction == 'S') direction = 'W';
-            else if (direction == 'W') direction = 'N';
-        } else if (command == 'F'){ // MOVE FORWARD
-            if (direction == 'N') row--;
-            if (direction == 'S') row++;
-            if (direction == 'W') column--;
-            if (direction == 'E') column++;
-        }
+        if (command == 'L')
+            rotateLeft();
+        else if (command == 'R')
+            rotateRight();
+        else if (command == 'F')
+            moveForward();
     }
+
+    private void moveForward() {
+        if (direction == 'N') row--;
+        else if (direction == 'S') row++;
+        else if (direction == 'W') column--;
+        else if (direction == 'E') column++;
+    }
+
+    private void rotateRight() {
+        if (direction == 'N') direction = 'E';
+        else if (direction == 'E') direction = 'S';
+        else if (direction == 'S') direction = 'W';
+        else if (direction == 'W') direction = 'N';
+    }
+
+    private void rotateLeft() {
+        if (direction == 'N') direction = 'W';
+        else if (direction == 'W') direction = 'S';
+        else if (direction == 'S') direction = 'E';
+        else if (direction == 'E') direction = 'N';
+    }
+
+
 }
